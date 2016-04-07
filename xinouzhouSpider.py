a197ef16d13ef08898c7a50c7bbb6bbff6b3bfdf
jianli zhang <jianli.zhang@havasmedia.com>
2016-04-07 13:35:28 +0200

    multithread for xinouzhouSpider

--- xinouzhouSpider.py
+++ xinouzhouSpider.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import string,urllib2,re
-def xinouzhou(url,begin_page,end_page):
-    f = open('test.txt','wb')
+import string,urllib2,re,threading
+
+class xinouzhouThread (threading.Thread):
+    def __init__(self, threadID, name, url,index,file):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.url=url
+        self.index=index
+        self.file=file
+    def run(self):
+        print "Starting " + self.name
+        readHtml(self.url,self.index,self.file)
+        print "Exiting " + self.name
+
+def xinouzhouSpider(url,begin_page,end_page):
+    f = open('C:/Users/jzhang/PycharmProjects/WedSpiderTutorial/test.txt','wb')
     for i in range(begin_page, end_page+1):
-        m = urllib2.urlopen(url + str(i)).read()
-        matches = re.findall(r']</em><a href=\"(.*?)\" onclick=\"atarget\(this\)\" class=\"s xst\">(.*?)</a>',m,re.DOTALL)
+        thread = xinouzhouThread(1, "Thread-"+str(i), url,i,f)
+        thread.start()
+    print "Exiting Main Thread"
 
-        #matches = re.findall(r'<img src=(.*)>',m,re.DOTALL)
-        for st in matches:
-            if "defense" in st[1]:
-                print(st[1])
-                f.write(st[1]+' ')
-                f.write(st[0]+'\n')
+def readHtml(url,index,file):
+     m = urllib2.urlopen(url + str(index)).read()
+     matches = re.findall(r']</em><a href=\"(.*?)\" onclick=\"atarget\(this\)\" class=\"s xst\">(.*?)</a>',m,re.DOTALL)
 
+     #matches = re.findall(r'<img src=(.*)>',m,re.DOTALL)
+     for st in matches:
+        if "defense" in st[1]:
+            print(st[1])
+            file.write(st[1]+' ')
+            file.write(st[0]+'\n')
 
-xinouzhou('http://buy.xineurope.com/forum.php?mod=forumdisplay&fid=10&cityid=1&typeid=18&filter=cityid&cityid=1&typeid=18&page=',1,10)
+if __name__ == "__main__":
+    xinouzhouSpider('http://buy.xineurope.com/forum.php?mod=forumdisplay&fid=10&cityid=1&typeid=18&filter=cityid&cityid=1&typeid=18&page=',1,10)
